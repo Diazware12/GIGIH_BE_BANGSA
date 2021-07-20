d7faab8cdc5043bf107f7abdade5a92e96c87554
diazware12 <diazwalker98@gmail.com>
2021-07-20 05:37:10 -0700

    homework

--- module3/session1/db_connector.rb
+++ module3/session1/db_connector.rb
@@ -63,3 +63,55 @@ def get_all_items_categories_price
     end
     items
 end
+
+
+def get_selected_item(product_name)
+    client = create_db_client
+    rawData=client.query("""
+            select 
+                i.id as item_id,
+                i.name as item_name,
+                i.price as item_price,
+                c.id as category_id,
+                c.name as category_name
+            from items as i 
+            join item_categories as ic on i.id = ic.item_id
+            join categories as c on c.id = ic.category_id
+            where i.name like '%#{product_name}%' limit 1
+        """)
+    items = Array.new
+    rawData.each do |data|
+        category = Category.new(data["category_id"],data["category_name"])
+        item = Item.new(data["item_id"],data["item_name"],data["item_price"],category)
+        items.push(item)
+    end
+    items
+end
+
+def update_items(name,price,item_id,category_id)
+    client = create_db_client
+    client.query("""
+                update items set 
+                    name = '#{name}'
+                where id = #{item_id}
+        """)
+        
+    client.query("""
+            update item_categories set 
+                category_id = #{category_id}
+            where item_id = #{item_id}
+        """)
+end
+
+def delete(id,name)
+    client = create_db_client
+    client.query("""
+            delete from item_categories
+            where item_id = #{id}
+        """)
+        
+    client.query("""
+            delete from items
+            where id = #{id} and name = '#{name}'
+        """)
+end
