44afe30f80371c913e7fdb0df49adee63f290b54
diazware12 <diazwalker98@gmail.com>
2021-07-28 05:26:38 -0700

    latihan tdd 2

--- module4/session2/src/addnum.rb
+++ module4/session2/src/addnum.rb
@@ -5,19 +5,23 @@ class AddNum
     def addNumber
         result = Array.new
         temp = 0
-        (@number.length-1).downto(0) do |index|
-            num = index == (@number.length-1) ? @number[index] + 1 + temp : @number[index] + temp
-            if num > 9
-                result << num - 10
-                temp = 1
-            else
-                result << num
-                temp = 0
+        if @number.empty? or @number == [0]
+            [1]
+        else
+            (@number.length-1).downto(0) do |index|
+                num = index == (@number.length-1) ? @number[index] + 1 + temp : @number[index] + temp
+                if num > 9
+                    result << num - 10
+                    temp = 1
+                else
+                    result << num
+                    temp = 0
+                end
             end
+            if temp == 1 
+                result << temp
+            end
+            result.reverse 
         end
-        if temp == 1 
-            result << temp
-        end
-        result.reverse
     end 
 end
