f741ebb6023f4dccda0dbebf8d05bd11a67edd41
diazware12 <diazwalker98@gmail.com>
2021-08-19 20:17:14 -0700

    [TEST][GREEN] test trendingcontroller passed

--- final_project/spec/check_controller_trendingcontroller_spec.rb
+++ final_project/spec/check_controller_trendingcontroller_spec.rb
@@ -23,5 +23,46 @@ describe TrendingController do
         expect(result).to eq(expected_view)
       end
     end
+    context 'no searching_API' do
+      it 'should show user list' do
+        controller = TrendingController.new
+        params = {
+          'userId' => 1
+        }
+
+        getUser = User.getUserById(1)
+        hashtags = Hashtag.trending
+    
+        trendings = []
+    
+        hashtags.each do |hashtag|
+          trendings.push({
+            "hashtagId": hashtag.hashtagId,
+            "hashtagName": hashtag.hashtagName,
+            "dtm_crt": hashtag.dtm_crt,
+            "totalUsage": hashtag.totalUsage
+          })
+        end
+
+        response = {
+          'message' => 'Success',
+          'status' => 200,
+          'method' => 'POST',
+          'data' => {
+            "Current Log-In User" => {
+              "full_name": getUser.full_name,
+              "username": getUser.username,
+              "email": getUser.email,
+              "password": getUser.password,
+              "gender": getUser.gender
+            },
+            'trending list' => trendings
+          }
+        }
+
+        result = controller.trendingPage_API(params)
+        expect(result.to_json).to eq(response.to_json)
+      end
+    end
   end
 end
