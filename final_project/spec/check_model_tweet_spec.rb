1d456e61c25a307a73c7404f47c54e193af0217d
diazware12 <diazwalker98@gmail.com>
2021-08-19 23:03:25 -0700

    [TEST][GREEN] test delete tweet

--- final_project/spec/check_model_tweet_spec.rb
+++ final_project/spec/check_model_tweet_spec.rb
@@ -59,5 +59,39 @@ describe Tweet do
     end
   end
 
+  describe '#deletetweet' do
+    context 'true' do
+      it 'should be true' do
+        stub_client = double
+        stub_query = "delete from tweets where tweetId = 1;"
+
+        tweet = Tweet.new({
+            tweetId: 1,
+            userId: 2,
+            content: 'only for test purposes',
+            attachment: '/transaction/foo.jpg',
+            dtm_crt: '2020-12-23'
+        })
+
+        allow(Mysql2::Client).to receive(:new).and_return(stub_client)
+        expect(stub_client).to receive(:query).with(stub_query)
+        tweet.delete
+      end
+    end
+    context 'false' do
+      it 'should be false' do
+        stub_client = double
+        stub_query = "delete from tweets where tweetId = 1;"
+
+        tweet = Tweet.new({
+            tweetId: 1,
+            dtm_crt: '2020-12-23'
+        })
+
+        expect(tweet.valid?).to eq(false)
+      end
+    end
+  end
+
 end
 
